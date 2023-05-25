1e34adf4ab9cd1193be9a5d095cf1c9bda2f7bdf
JasmineKobata <jasmineorjasnice@gmail.com>
2023-05-25 12:07:20 -0700

    problem 392 solved

--- isSubsequence.java
+++ isSubsequence.java
@@ -1,5 +1,15 @@
 class Solution {
     public boolean isSubsequence(String s, String t) {
-        
+        int sIdx = 0, sLen = s.length(), tLen = t.length();
+        if (sLen == 0) { return true; }
+
+        for (int tIdx=0; tIdx < tLen; tIdx++) {
+            if (s.charAt(sIdx) == t.charAt(tIdx)) {
+                sIdx++;
+                if (sIdx >= sLen) { return true; }
+            }
+        }
+
+        return sIdx >= sLen;
     }
 }
