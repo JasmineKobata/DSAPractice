e689fe0fee54b8e187505dcf5c135b2e3c3c6306
JasmineKobata <jasmineorjasnice@gmail.com>
2023-05-25 16:01:04 -0700

    problem 1456 file created

--- MinAvgSubarray.java
+++ MinAvgSubarray.java
@@ -1,5 +1,17 @@
 class Solution {
     public double findMaxAverage(int[] nums, int k) {
-        
+        double maxAvg = 0, currentWindow = 0;
+        for (int i=0; i < k; i++) {
+            currentWindow += nums[i];
+        }
+        maxAvg = currentWindow / k;
+
+        for (int i=0; i+k < nums.length; i++) {
+            currentWindow -= nums[i];
+            currentWindow += nums[i+k];
+            double currentAvg = currentWindow / k;
+            maxAvg = Math.max(maxAvg, currentAvg);
+        }
+        return maxAvg;
     }
 }
