33d6ad5039e8238fa27c37efc390c69f75816285
JasmineKobata <jasmineorjasnice@gmail.com>
2023-05-25 15:27:16 -0700

    problem 1679 file created

--- MaxKSumPairs.java
+++ MaxKSumPairs.java
@@ -1,5 +1,20 @@
 class Solution {
     public int maxOperations(int[] nums, int k) {
-        
+        Arrays.sort(nums);
+        int numOfPairs = 0;
+
+        int head = 0, tail = nums.length-1;
+        while(head < tail) {
+            if (nums[head] + nums[tail] == k) {
+                numOfPairs++;
+                head++;
+                tail--;
+            } else if (nums[head] + nums[tail] > k) {
+                tail--;
+            } else {
+                head++;
+            }
+        }
+        return numOfPairs;
     }
 }
