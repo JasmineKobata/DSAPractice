507f02e1f4caf473926bcedf82986960a57506ef
JasmineKobata <jasmineorjasnice@gmail.com>
2023-05-25 13:21:46 -0700

    problem 1732 solved

--- HighestAltitude.java
+++ HighestAltitude.java
@@ -1,5 +1,13 @@
 class Solution {
     public int largestAltitude(int[] gain) {
-        
+        int currentAlt = 0;
+        int maxAlt = 0;
+
+        for (int i=0; i < gain.length; i++) {
+            currentAlt += gain[i];
+            maxAlt = Math.max(currentAlt, maxAlt);
+        }
+
+        return maxAlt;
     }
 }
