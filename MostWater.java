c8016015571f1beaaf544c168e5c9799f9242c34
JasmineKobata <jasmineorjasnice@gmail.com>
2023-05-25 15:06:16 -0700

    problem 11 solved

--- MostWater.java
+++ MostWater.java
@@ -1,5 +1,19 @@
 class Solution {
     public int maxArea(int[] height) {
+        int maxVol = 0;
+        int head = 0, tail = height.length-1;
 
+        while (head < tail) {
+            int currentVol = Math.min(height[head], height[tail]) * (tail - head);
+            maxVol = Math.max(maxVol, currentVol);
+
+            if (height[head] < height[tail]) {
+                head++;
+            } else {
+                tail--;
+            }
+        }
+
+        return maxVol;
     }
 }
