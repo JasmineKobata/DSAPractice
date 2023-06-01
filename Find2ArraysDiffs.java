6206a14c9fda15994c2cd3397fcf4c84e86ec15b
JasmineKobata <jasmineorjasnice@gmail.com>
2023-05-31 17:33:21 -0700

    problem 2215 started

--- Find2ArraysDiffs.java
+++ Find2ArraysDiffs.java
@@ -1,5 +1,17 @@
 class Solution {
     public List<List<Integer>> findDifference(int[] nums1, int[] nums2) {
+        List<List<Integer>> answer;
+
+        Set<Integer> nums1set = new HashSet<Integer>();
+        Set<Integer> nums2set = new HashSet<Integer>();
+
+        for (int i=0; i < nums1.length; i++) {
+            nums1set.add(nums1[i]);
+        }
+        for (int i=0; i < nums2.length; i++) {
+            nums2set.add(nums2[i]);
+        }
+
         
     }
 }
