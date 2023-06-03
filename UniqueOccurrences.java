b296e3e1e3c42a34f6776209d3dc9cfba0dcdaf6
JasmineKobata <jasmineorjasnice@gmail.com>
2023-06-02 19:59:25 -0700

    problem 1207 solved

--- UniqueOccurrences.java
+++ UniqueOccurrences.java
@@ -1,5 +1,20 @@
 class Solution {
     public boolean uniqueOccurrences(int[] arr) {
-        
+        HashMap<Integer, Integer> numOfOccurrences = new HashMap<Integer, Integer>();
+
+        for (int i=0; i < arr.length; i++) {
+            int count = numOfOccurrences.containsKey(arr[i]) ? numOfOccurrences.get(arr[i]) : 0;
+            numOfOccurrences.put(arr[i], count + 1);
+        }
+
+        Integer[] values = numOfOccurrences.values().toArray(Integer[]::new);
+        Set<Integer> occurrences = new HashSet<Integer>();
+        for (int i=0; i < values.length; i++) {
+            if (occurrences.contains(values[i])) {
+                return false;
+            }
+            occurrences.add(values[i]);
+        }
+        return true;
     }
 }
