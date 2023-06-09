efcfa771b177ec5a1565ced51dead434df8dee76
JasmineKobata <jasmineorjasnice@gmail.com>
2023-06-09 14:03:30 -0700

    problem 1456 solved

--- MaxVowelsInSubstr.java
+++ MaxVowelsInSubstr.java
@@ -1,5 +1,20 @@
 class Solution {
     public int maxVowels(String s, int k) {
-        
+        Set<Character> vowels = new HashSet<Character>(Arrays.asList('a', 'e', 'i', 'o', 'u'));
+        int maxNum = 0;
+
+        for (int i=0; i < k; i++) {
+            if (vowels.contains(s.charAt(i))) { maxNum++; }
+        }
+
+        int currNum = maxNum;
+        for (int i=k; i < s.length(); i++) {
+            if (vowels.contains(s.charAt(i-k))) { currNum--; }
+            if (vowels.contains(s.charAt(i))) { currNum++; }
+
+            maxNum = Math.max(maxNum, currNum);
+        }
+
+        return maxNum;
     }
 }
