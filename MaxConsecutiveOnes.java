d3235ccc4d7aceb68461923fc672dcc37d17ca77
JasmineKobata <jasmineorjasnice@gmail.com>
2023-06-09 18:09:58 -0700

    problem 1004 solved

--- MaxConsecutiveOnes.java
+++ MaxConsecutiveOnes.java
@@ -1,5 +1,16 @@
 class Solution {
     public int longestOnes(int[] nums, int k) {
-        
+        int start = 0;
+        int end = 0;
+
+        for (; end < nums.length; end++) {
+            if (nums[end] == 0) k--;
+            if (k < 0) {
+                if (nums[start] == 0) k++;
+                start++;
+            }
+        }
+
+        return end - start;
     }
 }
