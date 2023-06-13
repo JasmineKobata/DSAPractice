5cf488751ac7cefdce147f22d601c14e1e6fe217
JasmineKobata <jasmineorjasnice@gmail.com>
2023-06-12 17:39:39 -0700

    problem 2352 solved

--- 2352EqualRowAndCol.java
+++ 2352EqualRowAndCol.java
@@ -1,5 +1,26 @@
 class Solution {
     public int equalPairs(int[][] grid) {
-        
+        HashMap<String, Integer> row = new HashMap<String, Integer>();
+        int numOfPairs = 0;
+
+        for (int i=0; i < grid.length; i++) {
+            String s = Arrays.toString(grid[i]);
+
+            if (!row.containsKey(s)) row.put(s, 0);
+            row.put(s, row.get(s) + 1);
+        }
+
+        for (int j=0; j < grid[0].length; j++) {
+            String s = "[";
+            for (int i=0; i < grid.length; i++) {
+                s += Integer.toString(grid[i][j]);
+                if (i < grid.length - 1) s += ", ";
+            }
+            s += "]";
+
+            if (row.containsKey(s)) numOfPairs += row.get(s); 
+        }
+
+        return numOfPairs;
     }
 }
