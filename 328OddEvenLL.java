50be3975872046d22324aca6e2eb28149d7d2a00
JasmineKobata <jasmineorjasnice@gmail.com>
2023-06-27 12:44:50 -0700

    problem 328 refactored

--- 328OddEvenLL.java
+++ 328OddEvenLL.java
@@ -2,25 +2,20 @@ class Solution {
     public ListNode oddEvenList(ListNode head) {
         if (head == null || head.next == null || head.next.next == null) return head;
         ListNode oddTail = head;
-        ListNode oddIt = head.next;
+        ListNode iter = head.next;
         ListNode evenHead = head.next;
 
-        while (oddIt != null && oddIt.next != null) {
-            //1 -> 3 -> 2 -> 4 -> 5
+        while (oddIt != null && iter.next != null) {
             evenHead = oddTail.next;
-            oddTail.next = oddIt.next;
-            //1 -> 3 -> 5
-            //2 -> 4 -> 5
-            //1 -> 3 -> 5 -> 2 -> 4
-            ListNode temp = oddIt; // 4
-            oddIt = oddIt.next; // 5
-            temp.next = oddIt.next; // null
-            oddIt.next = evenHead; // 4 -> null
-            //1 -> 3 -> 5 -> 4 -> null
-            oddTail = oddTail.next; // 3
-            oddIt = temp.next; // 4
-            System.out.println(oddTail.val);
-            if (oddIt != null) System.out.println(oddIt.val);
+            oddTail.next = iter.next;
+
+            ListNode evenTail = iter;
+            iter = iter.next;
+            evenTail.next = iter.next
+            iter.next = evenHead;
+
+            oddTail = oddTail.next;
+            iter = evenTail.next;
         }
 
         return head;
