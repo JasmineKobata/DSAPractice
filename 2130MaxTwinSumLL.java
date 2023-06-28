2f0af1405f3162f874602a9813882797a50c4cea
JasmineKobata <jasmineorjasnice@gmail.com>
2023-06-28 11:07:54 -0700

    problem 2130 solved

--- 2130MaxTwinSumLL.java
+++ 2130MaxTwinSumLL.java
@@ -1,5 +1,21 @@
 class Solution {
     public int pairSum(ListNode head) {
-        
+        if (head == null || head.next == null) return -1;
+        HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
+        ListNode slow = head;
+        ListNode fast = head;
+
+        int i = 0, result = 0;
+        while (fast != null && fast.next != null) {
+            map.put(i++, slow.val);
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        while (slow != null) {
+            result = Math.max(result, map.get(--i) + slow.val);
+            slow = slow.next;
+        }
+        return result;
     }
 }
