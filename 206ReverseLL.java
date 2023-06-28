6d206f3a0a0605945005d249c1afca24bb0e14a7
JasmineKobata <jasmineorjasnice@gmail.com>
2023-06-27 17:09:37 -0700

    problem 206 solved

--- 206ReverseLL.java
+++ 206ReverseLL.java
@@ -1,5 +1,16 @@
 class Solution {
     public ListNode reverseList(ListNode head) {
-        
+        if (head == null || head.next == null) return head;
+
+        ListNode tail = head;
+
+        while (tail.next != null) {
+            ListNode swappedNode = tail.next;
+            tail.next = tail.next.next;
+            swappedNode.next = head;
+            head = swappedNode;
+        }
+
+        return head;
     }
 }
