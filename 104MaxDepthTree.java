36c42fe1ce2c992f46dcddae96404926c1396c8d
JasmineKobata <jasmineorjasnice@gmail.com>
2023-06-28 14:07:44 -0700

    problem 104 solved

--- 104MaxDepthTree.java
+++ 104MaxDepthTree.java
@@ -1,5 +1,7 @@
 class Solution {
     public int maxDepth(TreeNode root) {
-        
+        if (root == null) return 0;
+
+        return Math.max(1 + maxDepth(root.left), 1 + maxDepth(root.right));
     }
 }
