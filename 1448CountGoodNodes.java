b2414a376b9becbb10ce8b9e88daea2765f09552
JasmineKobata <jasmineorjasnice@gmail.com>
2023-07-03 11:54:40 -0700

    problem 1448 solved

--- 1448CountGoodNodes.java
+++ 1448CountGoodNodes.java
@@ -1,5 +1,14 @@
 class Solution {
     public int goodNodes(TreeNode root) {
-        
+        return itNode(root, root.val);
+    }
+
+    public int itNode(TreeNode root, int maxNum) {
+        if (root == null) return 0;
+
+        if (root.val >= maxNum) {
+            return 1 + itNode(root.left, root.val) + itNode(root.right, root.val);
+        }
+        return itNode(root.left, maxNum) + itNode(root.right, maxNum);
     }
 }
