b1256880bf15e5f87e3c70ed8869364bcf839b0a
JasmineKobata <jasmineorjasnice@gmail.com>
2023-07-10 22:39:46 -0700

    problem 1372 attempted

--- 1372BinaryTreeZigzag.java
+++ 1372BinaryTreeZigzag.java
@@ -1,5 +1,23 @@
 class Solution {
     public int longestZigZag(TreeNode root) {
-        
+        char c = 'e'; //looking for e = either, l = left, r = right node
+        return parseTree(root, c, 0);
+    }
+
+    public int parseTree(TreeNode root, char c, int startVal) {
+        if (root == null) return -1;
+
+        int rStartVal = 0;
+        int lStartVal = 0;
+        if (c == 'e') {
+            
+        } else if (c == 'l') {
+            lStartVal = startVal;
+        } else if (c == 'r') {
+            rStartVal = startVal;
+        }
+
+        return Math.max(1 + parseTree(root.left, 'r', lStartVal),
+            1 + parseTree(root.right, 'l', rStartVal));
     }
 }
