46ff50c0295521753af3c03be6f3e5011f2d98d9
JasmineKobata <jasmineorjasnice@gmail.com>
2023-07-11 16:16:38 -0700

    problem 1372 solved

--- 1372BinaryTreeZigzag.java
+++ 1372BinaryTreeZigzag.java
@@ -1,22 +1,32 @@
 class Solution {
+    Integer max = 0;
+
     public int longestZigZag(TreeNode root) {
         char c = 'e'; //looking for e = either, l = left, r = right node
-        return parseTree(root, c, 0);
+        parseTree(root, c, 0);
+        return max;
     }
 
-    public int parseTree(TreeNode root, char c, int startVal) {
-        if (root == null) return -1;
-
-        int rStartVal = 0;
-        int lStartVal = 0;
-        if (c == 'e' || c == 'l') {
-            lStartVal = parseTree(root.left, 'r', startVal);
-        }
-        if (c == 'e' || c == 'r') {
-            rStartVal = parseTree(root.right, 'l', startVal);
+    public void parseTree(TreeNode root, char c, int val) {
+        if (root == null) {
+            // max = Math.max(val, max);
+            return;
         }
 
-        return Math.max(1 + lStartVal,
-            1 + rStartVal);
+        if (c == 'e') {
+            parseTree(root.left, 'r', val + 1);
+            parseTree(root.right, 'l', val + 1);
+        }
+        if (c == 'l') {
+            parseTree(root.left, 'r', val + 1);
+            parseTree(root.right, 'l', 1);
+        }
+        if (c == 'r') {
+            parseTree(root.left, 'r', 1);
+            parseTree(root.right, 'l', val + 1);
+        }
+
+        max = Math.max(val, max);
+        return;
     }
 }
