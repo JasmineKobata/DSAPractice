0c295d273fc3cf25b1e5e15dd5acac92005e046f
JasmineKobata <jasmineorjasnice@gmail.com>
2023-07-12 16:08:44 -0700

    problem 236 first attempt

--- 236LowestCommonAncestor.java
+++ 236LowestCommonAncestor.java
@@ -1,18 +1,23 @@
 class Solution {
+    TreeNode val = new TreeNode();
     int found = 0;
-    int val = 0;
 
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        
+        parseTree(root, p.val, q.val);
+        return val;
     }
 
-    public void parseTree(TreeNode root1, TreeNode root2, TreeNode p, TreeNode q) {
+    public void parseTree(TreeNode root, int p, int q) {
         if (root == null) return;
 
-        if (root == p || root == q) {
-            found++;
-        }
-        if (found == 2)
+        parseTree(root.left, p, q);
+        parseTree(root.right, p, q);
+        System.out.print(root.val);
+        System.out.print(" ");
+        System.out.println(found);
+        if (found == 2) val = root;
+        if (root.val == p || root.val == q || found == 2) found++;
+
         return;
     }
 }
