e0da5a7707107888568f48aceed4c0d97feeb861
JasmineKobata <jasmineorjasnice@gmail.com>
2023-07-20 15:27:47 -0700

    problem 700 solved

--- 700SearchBST.java
+++ 700SearchBST.java
@@ -1,5 +1,12 @@
 class Solution {
     public TreeNode searchBST(TreeNode root, int val) {
-        
+        if (root == null || root.val == val) return root;
+
+        TreeNode left = searchBST(root.left, val);
+        TreeNode right = searchBST(root.right, val);
+        TreeNode result = null;
+        if (left != null) result = left;
+        if (right != null) result = right;
+        return result;
     }
 }
