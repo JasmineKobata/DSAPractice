5156bfb0257e7b04304fd9b254b068383a1a66df
JasmineKobata <jasmineorjasnice@gmail.com>
2023-07-21 11:06:38 -0700

    problem 450 solved

--- 450DeleteBSTNode.java
+++ 450DeleteBSTNode.java
@@ -1,21 +1,31 @@
 class Solution {
-	public TreeNode deleteNode(TreeNode root, int key) {
-        if (root == null) return null;
-        else if (root.left != null && root.left.val == key) {
-            root.left = processNode(root.left);
-            return root;
-        } else if (root.right != null && root.right.val == key) {
-            root.right = processNode(root.right);
-            return root;
-        }
+    public TreeNode deleteNode(TreeNode root, int key) {
+        if (root == null) return root;
+
 
         if (key < root.val) {
-            return deleteNode(root.left, key);
+            root.left = deleteNode(root.left, key);
+        } else if (key > root.val) {
+            root.right = deleteNode(root.right, key);
         }
-        return deleteNode(root.right, key);
+        else {
+            if (root.left == null) {
+                return root.right;
+            } else if (root.right == null) {
+                return root.left;
+            }
+
+            TreeNode minNode = minNode(root.right);
+            root.val = minNode.val;
+            root.right = deleteNode(root.right, root.val);
+        }
+        return root;
     }
 
-    public TreeNode processNode(TreeNode root) {
-        return root.right != null ? root.right : root.left;
+    public TreeNode minNode(TreeNode root) {
+        while (root.left != null) {
+            root = root.left;
+        }
+        return root;
     }
 }
