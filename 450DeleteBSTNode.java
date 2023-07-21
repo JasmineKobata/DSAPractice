d116783509193a2fc01f74e2bf1535362e64b12d
JasmineKobata <jasmineorjasnice@gmail.com>
2023-07-20 21:37:53 -0700

    problem 450 attempted

--- 450DeleteBSTNode.java
+++ 450DeleteBSTNode.java
@@ -1,5 +1,21 @@
 class Solution {
-    public TreeNode deleteNode(TreeNode root, int key) {
-        
+	public TreeNode deleteNode(TreeNode root, int key) {
+        if (root == null) return null;
+        else if (root.left != null && root.left.val == key) {
+            root.left = processNode(root.left);
+            return root;
+        } else if (root.right != null && root.right.val == key) {
+            root.right = processNode(root.right);
+            return root;
+        }
+
+        if (key < root.val) {
+            return deleteNode(root.left, key);
+        }
+        return deleteNode(root.right, key);
+    }
+
+    public TreeNode processNode(TreeNode root) {
+        return root.right != null ? root.right : root.left;
     }
 }
