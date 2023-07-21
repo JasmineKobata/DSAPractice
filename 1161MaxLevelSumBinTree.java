3e50b43ce35c541cfe3efc3a7bdd1db0ba51a56c
JasmineKobata <jasmineorjasnice@gmail.com>
2023-07-20 22:26:10 -0700

    problem 1161 solved

--- 1161MaxLevelSumBinTree.java
+++ 1161MaxLevelSumBinTree.java
@@ -1,5 +1,30 @@
 class Solution {
     public int maxLevelSum(TreeNode root) {
-        
+        Queue<TreeNode> queue = new LinkedList<TreeNode>(Arrays.asList(root));
+        int layer = 1;
+        int resultSum = Integer.MIN_VALUE;
+        int resultLayer = 0;
+        int currentSum = 0;
+        int size = queue.size();
+
+        while (!queue.isEmpty()) {
+            TreeNode node = queue.remove();
+            currentSum += node.val;
+            size--;
+            if (node.right != null) queue.add(node.right);
+            if (node.left != null) queue.add(node.left);
+
+            if (size == 0) {
+                size = queue.size();
+                if (currentSum > resultSum) {
+                    resultSum = currentSum;
+                    resultLayer = layer;
+                }
+                layer++;
+                currentSum = 0;
+            }
+        }
+
+        return resultLayer;
     }
 }
