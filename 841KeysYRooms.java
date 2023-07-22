51866be8a3bc7f844965535ecae38ecc7848f11f
JasmineKobata <jasmineorjasnice@gmail.com>
2023-07-21 17:43:01 -0700

    problem 841 solved

--- 841KeysYRooms.java
+++ 841KeysYRooms.java
@@ -1,5 +1,22 @@
 class Solution {
     public boolean canVisitAllRooms(List<List<Integer>> rooms) {
-        
+        if (rooms.isEmpty()) return true;
+        Set<Integer> visited = new HashSet<Integer>(Arrays.asList(0));
+        Queue<List> queue = new LinkedList<List>();
+        queue.add(rooms.get(0));
+
+        while (!queue.isEmpty()) {         
+            List<Integer> currKey = queue.remove();
+
+            for (int i=0; i<currKey.size(); i++) {
+                int roomNum = currKey.get(i);
+                if (!visited.contains(roomNum)) {
+                    visited.add(roomNum);
+                    queue.add(rooms.get(roomNum));
+                }
+            }
+        }
+
+        return visited.size() == rooms.size();
     }
 }
