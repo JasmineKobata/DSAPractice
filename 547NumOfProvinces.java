62c3ae203263a425e0b9f2a96c39fbc7aae871c1
JasmineKobata <jasmineorjasnice@gmail.com>
2023-07-25 16:10:21 -0700

    problem 547 attempted

--- 547NumOfProvinces.java
+++ 547NumOfProvinces.java
@@ -1,5 +1,17 @@
 class Solution {
     public int findCircleNum(int[][] isConnected) {
-        
+        int result = isConnected.length;
+        Set<Integer> indices = new HashSet<Integer>();
+
+        for (int i=0; i < isConnected.length; i++) {
+            for (int j=i+1; j < isConnected.length; j++) {
+                if (isConnected[i][j] == 1 && !indices.contains(j)) {
+                    indices.add(j);
+                    result--;
+                }
+            }
+        }
+
+        return result;
     }
 }
