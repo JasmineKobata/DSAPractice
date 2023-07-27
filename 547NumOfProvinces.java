8b061982abbd6362cf0f6a41e131b863cffd0829
JasmineKobata <jasmineorjasnice@gmail.com>
2023-07-26 17:55:15 -0700

    problem 547 attempt 2

--- 547NumOfProvinces.java
+++ 547NumOfProvinces.java
@@ -1,15 +1,29 @@
+import java.util.Map.*;
+
 class Solution {
     public int findCircleNum(int[][] isConnected) {
-        int result = isConnected.length;
-        Set<Integer> indices = new HashSet<Integer>();
+        HashMap<Integer, Set<Integer>> indices = new HashMap<>();
 
         for (int i=0; i < isConnected.length; i++) {
-            for (int j=i+1; j < isConnected.length; j++) {
-                if (isConnected[i][j] == 1 && !indices.contains(j)) {
-                    indices.add(j);
-                    result--;
+            int index = findIdx(indices, i);
+            System.out.println(index);
+            for (int j=0; j < isConnected.length; j++) {
+                if (isConnected[i][j] == 1) {
+                    if (!indices.containsKey(index))
+                        indices.put(index, new HashSet<>());
+                    indices.get(index).add(j);                        
                 }
             }
+        }
+
+        return indices.size();
+    }
+
+    public int findIdx(HashMap<Integer, Set<Integer>> indices, int row) {
+        int result = row;
+
+        for (Entry<Integer, Set<Integer>> e : indices.entrySet()) {
+            if (e.getValue().contains(row)) result = e.getKey();
         }
 
         return result;
