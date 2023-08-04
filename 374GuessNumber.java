ad85825c36d25220d9477c0bd75331b7afc9bb78
JasmineKobata <jasmineorjasnice@gmail.com>
2023-08-04 16:07:21 -0700

    problem 374 attempted

--- 374GuessNumber.java
+++ 374GuessNumber.java
@@ -1,5 +1,21 @@
 public class Solution extends GuessGame {
     public int guessNumber(int n) {
-        
+        int start = 0;
+        int end = n;
+        while (start < end) {
+            int mid = start + (end - start) / 2;
+            System.out.print(start);
+            System.out.print(" ");
+            System.out.println(end);
+            if (guess(mid) == 1) {
+                start = mid;
+            } else if ( guess(mid) == -1) {
+                end = mid;
+            } else {
+                System.out.println("A");
+                return mid;
+            }
+        }
+        return start;
     }
 }
