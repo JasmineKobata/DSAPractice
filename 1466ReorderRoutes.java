f450e5a7ba559441ee6d1520c6bec41640d079b9
JasmineKobata <jasmineorjasnice@gmail.com>
2023-08-04 15:26:06 -0700

    problem 1466 refactored

--- 1466ReorderRoutes.java
+++ 1466ReorderRoutes.java
@@ -1,11 +1,10 @@
 class Solution {
-    public int dfs(ArrayList<int[]>[] connections, int i, Set<Integer> vis) {
+    public int dfs(ArrayList<int[]>[] connections, int i, Set<Integer> vis, int swaps) {
         vis.add(i);
-        int swaps = 0;
 
         for (int[] elem: connections[i]) {
             if (!vis.contains(elem[0])) {
-                swaps += dfs(connections, elem[0], vis) + elem[1];
+                swaps = dfs(connections, elem[0], vis, swaps + elem[1]);
             }
         }
 
@@ -20,21 +19,11 @@ class Solution {
             array[i] = new ArrayList<>();
         }
 
-        System.out.println(Arrays.toString(array));
-
         for (int[] elem : connections) {
             array[elem[0]].add(new int[]{elem[1], 1});
             array[elem[1]].add(new int[]{elem[0], 0});
         }
 
-        for (int i=0; i<n; i++) {
-            for (int[] arr : array[i]) {
-                System.out.print(Arrays.toString(arr));
-                System.out.print(" ");                         
-            }
-            System.out.println();
-        }
-
-        return dfs(array, 0, visited);
+        return dfs(array, 0, visited, 0);
     }
 }
