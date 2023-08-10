731f0513bf7bab91f4dfa0523f44319f72a42bc1
JasmineKobata <jasmineorjasnice@gmail.com>
2023-08-09 17:11:19 -0700

    problem 399 attempt 2

--- 399EvaluateDiv.java
+++ 399EvaluateDiv.java
@@ -15,21 +15,32 @@ class Solution {
 
         int i = 0;
         for (List<String> q : queries) {
-            System.out.println(i);
             if (!map.containsKey(q.get(0)) || !map.containsKey(q.get(1))) {
                 result[i++] = -1.0;
             } else {
-                result[i++] = dfs(map, q); //recursively find multiplication path                
+                result[i++] = dfs(map, q, q); //recursively find multiplication path                
             }
         }
 
         return result;
     }
 
-    public double dfs(HashMap<String, HashMap<String, Double>> map, List<String> q) {
-        map.get(q.get(0)).forEach( (k, v) -> {
-            System.out.println(k);
-        });
-        return 0;
+    public double dfs(HashMap<String, HashMap<String, Double>> map, List<String> q, List<String> iter) {
+        double result = 0;
+        System.out.println(q);
+        // map.get(iter.get(0)).forEach( (k, v) -> {
+        //     //add v to result if first # found or both #s found
+        //     // result += dfs(map, q, new List<>(''))
+        // });
+
+        for ( String key : map.get(iter.get(0)).keySet() ) {
+            System.out.println(key);
+            if ( key == q.get(1) ) {
+                System.out.println(map.get(key).get(iter.get(1)));
+            }
+            // System.out.println(key);
+        }
+
+        return result;
     }
 }
