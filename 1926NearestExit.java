a8e4ae8d041bcb67bd9f1a1f669a6d98a8d9f474
JasmineKobata <jasmineorjasnice@gmail.com>
2023-08-17 16:14:10 -0700

    problem 1926 compiles

--- 1926NearestExit.java
+++ 1926NearestExit.java
@@ -1,7 +1,8 @@
 class Solution {
     public int nearestExit(char[][] maze, int[] entrance) {
         Set<List<Integer>> visited = new HashSet();
-        visited.add(Arrays.asList(entrance));
+        List<Integer> list = Arrays.stream(entrance).boxed().toList();
+        visited.add(list);
         System.out.println(visited);
         return bfs(maze, visited, entrance);
     }
@@ -15,18 +16,19 @@ class Solution {
         int[] up = new int[]{x, y-1};
         int[] down = new int[]{x, y+1};
 
-        if (x-1 >= 0 && !visited.includes(Arrays.asList(left))) {
+        if (x-1 >= 0 && !visited.contains(Arrays.asList(left))) {
             return 1 + bfs(maze, visited, left);
         }
-        if (y-1 >= 0 && !visited.includes(Arrays.asList(up))) {
+        if (y-1 >= 0 && !visited.contains(Arrays.asList(up))) {
             return 1 + bfs(maze, visited, up);
         }
-        if (x+1 >= 0 && !visited.includes(Arrays.asList(right))) {
+        if (x+1 >= 0 && !visited.contains(Arrays.asList(right))) {
             return 1 + bfs(maze, visited, right);
         }
-        if (y+1 >= 0 && !visited.includes(Arrays.asList(down))) {
+        if (y+1 >= 0 && !visited.contains(Arrays.asList(down))) {
             return 1 + bfs(maze, visited, down);
         }
+        return -1;
     }
 
     public boolean isBorder(int[] currentSqr, char[][] maze) {
