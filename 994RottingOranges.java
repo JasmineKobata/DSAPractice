d45c82279204c6d4d35649fac6a7b8f73c4498cd
JasmineKobata <jasmineorjasnice@gmail.com>
2023-08-18 15:36:51 -0700

    problem 994 solved brute force method

--- 994RottingOranges.java
+++ 994RottingOranges.java
@@ -1,17 +1,24 @@
 class Solution {
     public int orangesRotting(int[][] grid) {
-        int minutes = 0;
-        boolean allRotten = false;
+        int minutes = -1;
+        Boolean allRotten = false;
 
         while (!allRotten) {
             allRotten = true;
+            minutes++;
+            int[][] tempGrid = copyArray(grid);
             for (int i=0; i < grid.length; i++) {
                 for (int j=0; j < grid[i].length; j++) {
                     if (grid[i][j] == 2) {
-                        allRotten = updateGrid(grid, i, j);
+                        allRotten &= updateGrid(grid, tempGrid, i, j);
                     }
                 }
             }
+    for (int x=0; x < grid.length; x++) {
+        System.out.print(Arrays.stream(grid[x]).boxed().toList().toString());
+        System.out.println(Arrays.stream(tempGrid[x]).boxed().toList().toString());}
+
+        grid = copyArray(tempGrid);
         }
 
         for (int i=0; i < grid.length; i++) {
@@ -23,32 +30,32 @@ class Solution {
         return minutes;
     }
 
-    public boolean updateGrid(int[][] grid, int x, int y) {
+    public boolean updateGrid(int[][] grid, int[][] tempGrid, int x, int y) {
         int[] left = new int[]{x-1, y};
         int[] right = new int[]{x+1, y};
         int[] up = new int[]{x, y-1};
         int[] down = new int[]{x, y+1};
 
-        boolean freshFound = false;
+        boolean allRotten = true;
 
         if (inBounds(left, grid)) {
-            grid[left[0]][left[1]] = 2;
-            freshFound = true;
+            tempGrid[left[0]][left[1]] = 2;
+            allRotten &= false;
         }
         if (inBounds(right, grid)) {
-            grid[right[0]][right[1]] = 2;
-            freshFound = true;
+            tempGrid[right[0]][right[1]] = 2;
+            allRotten &= false;
         }
         if (inBounds(up, grid)) {
-            grid[up[0]][up[1]] = 2;
-            freshFound = true;
+            tempGrid[up[0]][up[1]] = 2;
+            allRotten &= false;
         }
         if (inBounds(down, grid)) {
-            grid[down[0]][down[1]] = 2;
-            freshFound = true;
+            tempGrid[down[0]][down[1]] = 2;
+            allRotten &= false;
         }
 
-        return freshFound;
+        return allRotten;
     }
 
     public boolean inBounds(int[] sqr, int[][] grid) {
@@ -56,5 +63,16 @@ class Solution {
 
         return x >= 0 && x < grid.length && y >= 0 && y < grid[0].length
             && grid[x][y] == 1;
+    }
+
+    public int[][] copyArray(int[][] grid) {
+        int[][] copy = new int[grid.length][grid[0].length];
+
+        for (int i=0; i < copy.length; i++) {
+            for (int j=0; j < copy[0].length; j++) {
+                copy[i][j] = grid[i][j];
+            }
+        }
+        return copy;
     }
 }
