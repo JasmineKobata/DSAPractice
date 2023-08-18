6153f2cf2b3a80e95ee83b8e9d8fc22ea0e43ff6
JasmineKobata <jasmineorjasnice@gmail.com>
2023-08-18 13:27:26 -0700

    problem 1926 stuck at testcase 104

--- 1926NearestExit.java
+++ 1926NearestExit.java
@@ -1,19 +1,17 @@
 class Solution {
     Set<String> visited = new HashSet();
+    boolean edgeFound = false;
 
     public int nearestExit(char[][] maze, int[] entrance) {
-        System.out.println("Start: " + maze.toString());
         int result = bfs(maze, entrance, entrance);
-        System.out.println("End: " + result);
-        return result;
+        return edgeFound ? result : -1;
     }
 
     public int bfs(char[][] maze, int[] currentSqr, int[] startSqr) {
-        System.out.println("VIS: " + visited.toString() + ' ');
         int startX = startSqr[0], startY = startSqr[1];
         int x = currentSqr[0], y = currentSqr[1];
         if ((startX != x || startY != y) && isBorder(currentSqr, maze)) {
-            System.out.println("Z");
+            edgeFound = true;
             return 0;
         }
 
@@ -32,21 +30,16 @@ class Solution {
         System.out.println(maze[x][y]);
 
         if (x-1 >= 0 && isPath(left, maze) && !visited.contains(leftStr)) {
-            System.out.println("A " + visited.toString());
             int num = 1 + bfs(maze, left, startSqr);
-            System.out.println("A " + num);
             return num;
         }
         if (y-1 >= 0  && isPath(up, maze) && !visited.contains(upStr)) {
-            System.out.println("B");
             return 1 + bfs(maze, up, startSqr);
         }
         if (x+1 < maze.length && isPath(right, maze) && !visited.contains(rightStr)) {
-            System.out.println("C");
             return 1 + bfs(maze, right, startSqr);
         }
         if (y+1 < maze[0].length && isPath(down, maze) && !visited.contains(downStr)) {
-            System.out.println("D");
             return 1 + bfs(maze, down, startSqr);
         }
         System.out.println("E");
@@ -59,7 +52,6 @@ class Solution {
     }
 
     public boolean isPath(int[] sqr, char[][] maze) {
-        System.out.println("PATH: " + Arrays.stream(sqr).boxed().toList().toString());
         return (maze[sqr[0]][sqr[1]] == '.');
     }
 }
