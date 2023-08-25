dd7af6c27b347dab512aa24affc69e99f2a4ae73
JasmineKobata <jasmineorjasnice@gmail.com>
2023-08-25 00:25:29 -0700

    problem 2300 32/56 testcases passed

--- 2300SuccessfulPairs.java
+++ 2300SuccessfulPairs.java
@@ -6,16 +6,20 @@ class Solution {
         System.out.println(Arrays.toString(potions));
 
         for (int i=0; i < spells.length; i++) {
-            int mid = (pLen) / 2;
+            int start = 0, end = pLen;
+            int mid = pLen / 2;
             System.out.println("A " + mid);
             while (mid < pLen && mid > 0
                 && (spells[i] * potions[mid] < success
                 || spells[i] * potions[mid-1] >= success)) {
                 if (spells[i] * potions[mid] < success) {
-                    mid = (pLen - mid) / 2 + mid + 1;
+                    mid = (end - mid - 1) / 2 + mid + 1;
                     System.out.println("Q " + mid);
                 }
-                else if (spells[i] * potions[mid] > success) mid /= 2;
+                else if (spells[i] * potions[mid] > success) {
+                    end = mid;
+                    mid /= 2;
+                }
                 System.out.println("B " + mid);
             }
             System.out.println("C " + pLen + " " + mid);
