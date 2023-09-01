467875ec611e4e044d9a4cea9e0b6078f0644c81
JasmineKobata <jasmineorjasnice@gmail.com>
2023-09-01 14:51:53 -0700

    problem 162 started

--- 162PeakElem.java
+++ 162PeakElem.java
@@ -1,5 +1,14 @@
 class Solution {
     public int findPeakElement(int[] nums) {
-        
+        int start = 1, end = nums.length-2;
+        while (start < end) {
+            int mid = start + (end - start) / 2;
+            
+            if (nums[i-1] < nums[i] && nums[i+1] < nums[i]) {
+                return mid;
+            }
+        }
+
+        return 0;
     }
 }
