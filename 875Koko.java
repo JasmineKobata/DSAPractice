ce43a311a8cca7576e04549675379dbf26b4d813
JasmineKobata <jasmineorjasnice@gmail.com>
2023-09-01 15:48:58 -0700

    problem 875 started

--- 875Koko.java
+++ 875Koko.java
@@ -1,5 +1,8 @@
 class Solution {
     public int minEatingSpeed(int[] piles, int h) {
-        
+        int start = 1, end = nums.length-1;
+        while (start < end) {
+            int mid = start + (end - start) / 2;
+        }
     }
 }
