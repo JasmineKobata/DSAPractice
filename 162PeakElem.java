0f5baee44b238116a4b6171e586d850c9f475986
JasmineKobata <jasmineorjasnice@gmail.com>
2023-09-01 15:28:06 -0700

    problem 162 first attempt

--- 162PeakElem.java
+++ 162PeakElem.java
@@ -1,10 +1,16 @@
 class Solution {
     public int findPeakElement(int[] nums) {
-        int start = 1, end = nums.length-2;
+        int start = 1, end = nums.length-1;
         while (start < end) {
             int mid = start + (end - start) / 2;
             
-            if (nums[i-1] < nums[i] && nums[i+1] < nums[i]) {
+            if (nums[mid-1] > nums[mid]) {
+                return findPeakElement(Arrays.copyOfRange(nums, start, mid));
+            }
+            if (nums[mid+1] > nums[mid]) {
+                return findPeakElement(Arrays.copyOfRange(nums, mid, end + 1));
+            }
+            if (nums[mid-1] < nums[mid] && nums[mid+1] < nums[mid]) {
                 return mid;
             }
         }
