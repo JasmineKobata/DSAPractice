4cf567c92724d98f5de52eeaa01ae0b3ce979cda
JasmineKobata <jasmineorjasnice@gmail.com>
2023-09-08 16:34:08 -0700

    problem 17 started

--- 17LetterCombosPhoneNo.java
+++ 17LetterCombosPhoneNo.java
@@ -1,5 +1,20 @@
 class Solution {
+    HashMap<Character, String> map = new HashMap<>(){{
+        put("2", "abc");
+        put("3", "def");
+        put("4", "ghi");
+        put("5", "jkl");
+        put("6", "mno");
+        put("7", "pqrs");
+        put("8", "tuv");
+        put("9", "wxyz");
+    }}
+
     public List<String> letterCombinations(String digits) {
-        
+        List<Character> strings = new List<>();
+
+        for (Character c : digits) {
+            
+        }
     }
 }
