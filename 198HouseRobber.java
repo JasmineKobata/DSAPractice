426a13d256993f59f2fc12bba361d1e94e8d6bf3
JasmineKobata <jasmineorjasnice@gmail.com>
2023-10-14 19:08:34 -0700

    problem 198 sovled

--- 198HouseRobber.java
+++ 198HouseRobber.java
@@ -3,11 +3,12 @@ class Solution {
         List<Integer> dp = new ArrayList<>();
 
         for (int i=0; i < nums.length; i++) {
-            if (i < 2)
+            if (i < 1)
                 dp.add(nums[i]);
-            else {
+            else if (i == 1)
+                dp.add(Math.max(nums[i], nums[i-1]));
+            else
                 dp.add(Math.max(dp.get(i-2) + nums[i], dp.get(i-1)));
-            }
         }
 
         return dp.get(dp.size() - 1);
