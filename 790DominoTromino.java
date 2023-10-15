f30aa537c3d9f878d0803f4655d30aeeb5ba0872
JasmineKobata <jasmineorjasnice@gmail.com>
2023-10-14 22:19:45 -0700

    problem 290 solved

--- 790DominoTromino.java
+++ 790DominoTromino.java
@@ -1,14 +1,14 @@
 class Solution {
     public int numTilings(int n) {
-        List<Integer> dp = new ArrayList<>();
-        dp.add(1);
-        dp.add(2);
-        dp.add(5);
+        List<Long> dp = new ArrayList<>();
+        dp.add(1L);
+        dp.add(2L);
+        dp.add(5L);
 
         for (int i=3; i < n; i++) {
             dp.add((dp.get(i-1)*2 + dp.get(i-3)) % 1000000007);
         }
-        System.out.println(dp);
-        return dp.get(dp.size() - 1);
+        System.out.println(dp.get(n - 1));
+        return dp.get(n - 1).intValue();
     }
 }
