ff8511369bac9a6f8ca591d9aebd5f3743ceaa65
JasmineKobata <jasmineorjasnice@gmail.com>
2023-10-16 18:01:03 -0700

    problem 1143 attempt 2

--- 1143LongestSubsequence.java
+++ 1143LongestSubsequence.java
@@ -2,7 +2,11 @@ class Solution {
     public int longestCommonSubsequence(String text1, String text2) {
         int[][] dp = new int[text1.length()][text2.length()];
         for (int x=0; x<text1.length(); x++) {
-            dp[x][0] = text1.charAt(x) == text2.charAt(0) ? 1 : 0;
+            if (text1.charAt(x) == text2.charAt(0))
+                dp[x][0] = 1;
+            else if (x > 0 && dp[x-1][0] == 1)
+                dp[x][0] = 1;
+            else dp[x][0] = 0;
         }
         for (int y=0; y<text2.length(); y++) {
             dp[0][y] = text2.charAt(y) == text1.charAt(0) ? 1 : 0;
@@ -12,12 +16,19 @@ class Solution {
             for (int y=1; y<text2.length(); y++) {
                 if (text1.charAt(x) == text2.charAt(y))
                     dp[x][y] = dp[x-1][y-1] + 1;
+                else if (x > 0 && dp[x-1][y] == 1)
+                    dp[x][y] = 1;
                 else
                     dp[x][y] = dp[x-1][y-1];
             }
         }
 
-        System.out.println(dp);
+        for (int x=0; x<text1.length(); x++) {
+            for (int y=0; y<text2.length(); y++) {
+                System.out.print(dp[x][y]);
+            }
+            System.out.println();
+        }
 
         return dp[text1.length()-1][text2.length()-1];
     }
