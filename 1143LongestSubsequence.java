95fe393f44d29986aca45750f397ec6122630bb5
JasmineKobata <jasmineorjasnice@gmail.com>
2023-10-16 18:22:57 -0700

    problem 1143 solved

--- 1143LongestSubsequence.java
+++ 1143LongestSubsequence.java
@@ -10,23 +10,29 @@ class Solution {
         }
         for (int y=0; y<text2.length(); y++) {
             dp[0][y] = text2.charAt(y) == text1.charAt(0) ? 1 : 0;
+
+            if (text2.charAt(y) == text1.charAt(0))
+                dp[0][y] = 1;
+            else if (y > 0 && dp[0][y-1] == 1)
+                dp[0][y] = 1;
+            else dp[0][y] = 0;
         }
 
         for (int x=1; x<text1.length(); x++) {
             for (int y=1; y<text2.length(); y++) {
-                if (text1.charAt(x) == text2.charAt(y))
-                    dp[x][y] = dp[x-1][y-1] + 1;
+                if (text1.charAt(x) == text2.charAt(y)) {
+                    dp[x][y] = dp[x-1][y-1] + 1;}
                 else
                     dp[x][y] = Math.max(dp[x-1][y], dp[x][y-1]);
             }
         }
 
-        for (int x=0; x<text1.length(); x++) {
-            for (int y=0; y<text2.length(); y++) {
-                System.out.print(dp[x][y]);
-            }
-            System.out.println();
-        }
+        // for (int x=0; x<text1.length(); x++) {
+        //     for (int y=0; y<text2.length(); y++) {
+        //         System.out.print(dp[x][y]);
+        //     }
+        //     System.out.println();
+        // }
 
         return dp[text1.length()-1][text2.length()-1];
     }
