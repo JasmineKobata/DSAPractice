d9ddedcfdb4daa8239980673bded52c3878b3e48
JasmineKobata <jasmineorjasnice@gmail.com>
2023-10-16 17:53:54 -0700

    problem 1143 attempt 1

--- 1143LongestSubsequence.java
+++ 1143LongestSubsequence.java
@@ -8,12 +8,16 @@ class Solution {
             dp[0][y] = text2.charAt(y) == text1.charAt(0) ? 1 : 0;
         }
 
+        for (int x=1; x<text1.length(); x++) {
+            for (int y=1; y<text2.length(); y++) {
+                if (text1.charAt(x) == text2.charAt(y))
+                    dp[x][y] = dp[x-1][y-1] + 1;
+                else
+                    dp[x][y] = dp[x-1][y-1];
+            }
+        }
 
-        // for (int x=0; x<text1.length(); x++) {
-        //     for (int y=0; y<text2.length(); y++) {
-
-        //     }
-        // }
+        System.out.println(dp);
 
         return dp[text1.length()-1][text2.length()-1];
     }
