2c77a25f90766048db701515fd24c9b9d7a35747
JasmineKobata <jasmineorjasnice@gmail.com>
2023-10-26 16:29:43 -0700

    problem 1542 attempt 1

--- 2542MaxSubsequenceScore.java
+++ 2542MaxSubsequenceScore.java
@@ -2,7 +2,8 @@ import java.util.*;
 
 class SortPairs implements Comparator<Vector<Integer>> {
     @Override public int compare(Vector<Integer> a, Vector<Integer> b) {
-        return b.get(0).compareTo(a.get(0));
+        // if (a.get(0) == b.get(0)) return a.get(1).compareTo(b.get(1));
+        return a.get(0).compareTo(b.get(0));
     }
 }
 
@@ -15,9 +16,24 @@ class Solution {
             v.add(new Vector<>(Arrays.asList(nums2[i], nums1[i])));
         }
         v.sort(new SortPairs());
+        long result = 0;
+        long sum = 0;
+        for (int i=nums1.length-1; i >= 0; i--) {
+            int num1 = v.get(i).get(1);
+            if (i > nums1.length - k) {
+                sum += num1;
+                pq.add(num1);
+            } else {
+                sum += num1;
+                pq.add(num1);
+                sum -= pq.remove();
+            }
+            System.out.println(pq);
+            result = Math.max(result, sum*v.get(i).get(0));
+        }
 
         System.out.println(v);
 
-        return 0L;
+        return result;
     }
 }
