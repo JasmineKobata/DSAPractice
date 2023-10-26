bf6f780b9a5f6c6104602517efbf213735c19b66
JasmineKobata <jasmineorjasnice@gmail.com>
2023-10-26 13:49:06 -0700

    class Solution {     public long maxScore(int[] nums1, int[] nums2, int k) {         PriorityQueue<Integer> pq = new PriorityQueue<>();         Vector<Vector<Integer>> v;
            for (int i=0; i < k; i++) {
                v.add({nums2[i], nums1[i]});
            }
    
            return 0L;
        }
    
    ;

--- 2542MaxSubsequenceScore.java
+++ 2542MaxSubsequenceScore.java
@@ -1,16 +1,12 @@
 class Solution {
     public long maxScore(int[] nums1, int[] nums2, int k) {
-        int n = nums1.length;
-        long[] nums21 = new long[n];
-        for (int i=0; i < n; i++) {
-            nums21[i] = ((long)nums2[i] << 20) | nums1[i];
-        }
-        System.out.println(Arrays.toString(nums21));
-        Arrays.sort(nums21);
-        System.out.println(Arrays.toString(nums21));
-
         PriorityQueue<Integer> pq = new PriorityQueue<>();
-        
-        return 0;
+        Vector<Vector<Integer>> v;        
+
+        for (int i=0; i < k; i++) {
+            v.add({nums2[i], nums1[i]});
+        }
+
+        return 0L;
     }
 }
