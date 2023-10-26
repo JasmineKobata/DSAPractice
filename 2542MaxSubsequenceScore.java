c0099d69b8f628850645d69dbbce2c3cbe1b57c7
JasmineKobata <jasmineorjasnice@gmail.com>
2023-10-26 14:37:53 -0700

    problem 1542 compiled

--- 2542MaxSubsequenceScore.java
+++ 2542MaxSubsequenceScore.java
@@ -1,11 +1,22 @@
+import java.util.*;
+
+class SortPairs implements Comparator<Vector<Integer>> {
+    @Override public int compare(Vector<Integer> a, Vector<Integer> b) {
+        return b.get(0).compareTo(a.get(0));
+    }
+}
+
 class Solution {
     public long maxScore(int[] nums1, int[] nums2, int k) {
         PriorityQueue<Integer> pq = new PriorityQueue<>();
-        Vector<Vector<Integer>> v;        
+        Vector<Vector<Integer>> v = new Vector<>();        
 
-        for (int i=0; i < k; i++) {
-            v.add({nums2[i], nums1[i]});
+        for (int i=0; i < nums1.length; i++) {
+            v.add(new Vector<>(Arrays.asList(nums2[i], nums1[i])));
         }
+        v.sort(new SortPairs());
+
+        System.out.println(v);
 
         return 0L;
     }
