2dea6c8f2db5cc9ec6c3dbfcf3b1fe4a69ea72fd
JasmineKobata <jasmineorjasnice@gmail.com>
2023-10-27 22:53:25 -0700

    problem 714 solved

---
+++ 714StockTransactonFee.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int maxProfit(int[] prices, int fee) {
+        int profit = 0;
+        int effectiveBuy = prices[0];
+
+        for (int i = 1; i < prices.length; i++) {
+            profit = Math.max(profit, prices[i] - effectiveBuy - fee);
+            effectiveBuy = Math.min(effectiveBuy, prices[i] - profit);
+        }
+        
+        return profit;
+    }
+}
