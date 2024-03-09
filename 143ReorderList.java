be6419fb76da2fc93d03ec9a1e7f6cef58c7ef7a
JasmineKobata <jasmineorjasnice@gmail.com>
2024-03-08 21:59:42 -0800

    problem 143 reattempted

--- 143ReorderList.java
+++ 143ReorderList.java
@@ -1,29 +1,15 @@
 class Solution {
     public void reorderList(ListNode head) {
-        ListNode slow = head, fast = head.next;
-        ListNode reverseList = null, result = null;
+        if (head == null || head.next == null) return;
+        ListNode slow = head, fast = head.next.next;
+        ListNode left = null, right = null;
 
-        while (fast.next != null && fast.next.next != null) {
-            ListNode temp = reverseList;
-            reverseList = slow;
-            reverseList.next = temp;
+        while (fast != null && fast.next != null) {
+            ListNode temp = left;
+            left = slow;
+            left.next = temp;
             slow = slow.next;
             fast = fast.next.next;
-        }
-        if (fast.next != null) {
-            ListNode temp = result;
-            result = slow.next;
-            result.next = temp;
-            slow = slow.next;
-        }
-        ListNode itEnd = slow, itStart = reverseList;
-        while (itEnd != null) {
-            ListNode temp = result;
-            ListNode tempEnd = itEnd;
-            ListNode tempStart = itStart;
-            itEnd = itEnd.next;
-            itStart = itStart.next;
-            
         }
     }
 }
