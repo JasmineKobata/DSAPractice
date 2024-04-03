2f6042913c1b580fc1b4faa4dbfa499e9b769031
JasmineKobata <jasmineorjasnice@gmail.com>
2024-04-03 13:30:21 -0700

    problem 110 refactored

--- 110BalancedBinTree.java
+++ 110BalancedBinTree.java
@@ -2,9 +2,6 @@ class Solution {
     public boolean isBalanced(TreeNode root) {
         if (root == null) return true;
 
-        isBalanced(root.left);
-        isBalanced(root.right);
-
         return isBalanced(root.left) && isBalanced(root.right) && Math.abs(maxHeight(root.left) - maxHeight(root.right)) <= 1;
     }
 
